8b32e799cd7cb8654445d2291245ef31c6ff2109
hamzaabbasi54 <hamza.abbasi3030@gmail.com>
2026-02-19 00:08:19 +0500

    login and signup working from frontend and toast is displaying info correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router'
+import { Toaster } from 'react-hot-toast'
 import Chatpage from './pages/Chat.jsx'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
@@ -7,6 +8,7 @@ function App() {
   return (
 
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
       {/* DECORATORS - GRID BG & GLOW SHAPES */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] pointer-events-none" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px] pointer-events-none" />
