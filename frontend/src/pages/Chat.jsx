336d5cb96e1f5e9c1a9558632a8c20ef6096de80
hamzaabbasi54 <hamza.abbasi3030@gmail.com>
2026-02-18 23:52:48 +0500

    backend functionality of signin and out using zustand

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import { useChatStore } from "../store/useChatStore";
+// import { useChatStore } from "../store/useChatStore";
 
 import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
 // import ProfileHeader from "../components/ProfileHeader";
@@ -9,25 +9,25 @@ import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
 // import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 
 function ChatPage() {
-  const { activeTab, selectedUser } = useChatStore();
+  // const { activeTab, selectedUser } = useChatStore();
 
   return (
     <div className="relative w-full max-w-6xl h-[800px]">
       <BorderAnimatedContainer>
         {/* LEFT SIDE */}
-        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+        {/* <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
           <ProfileHeader />
           <ActiveTabSwitch />
 
           <div className="flex-1 overflow-y-auto p-4 space-y-2">
             {activeTab === "chats" ? <ChatsList /> : <ContactList />}
           </div>
-        </div>
+        </div> */}
 
         {/* RIGHT SIDE */}
-        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+        {/* <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
           {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
-        </div>
+        </div> */}
       </BorderAnimatedContainer>
     </div>
   );
